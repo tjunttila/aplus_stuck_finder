a9cae41b97def79970d8f5ada10a56496ece211e
Tommi Junttila <Tommi.Junttila@aalto.fi>
2022-10-03 23:04:17 +0300

    Use A+ API differently, only consider recent submissions

--- README.md
+++ README.md
@@ -16,5 +16,6 @@ edit the `find_stuck_submissions.yml` file as follows.
 * The field `api-url` gives the API URL of the A+ installation in question. For instance, at Aalto University it is https://plus.cs.aalto.fi/api/v2/. Remember to include the trailing `/`.
 * The field `course-id` is the integer identifier of the course instance you wish to scan for stuck submissions. One can find the course instances in the A+ API. For instance, the Aalto installation has them listed at https://plus.cs.aalto.fi/api/v2/courses/.
 * The field `access-token` is your personal API access token (a sequence of characters and numbers) in the A+ installation. You can find it in your A+ profile information.
+
 **Warning:** as the access token grants *your* rights to the course instance,
 you should keep the `find_stuck_submissions.yml` file so that nobody else can access it by accident (set appropriate file premissions, do not commit it to a shared repository, and so on).
--- find_stuck_submissions.py
+++ find_stuck_submissions.py
@@ -6,10 +6,12 @@ Author: Tommi Junttila
 """
 
 import argparse
+import datetime
 import sys
 import time
 import requests
 import yaml
+from dateutil.parser import parse as timeparser
 
 
 def api_get_url_json(url, access_token, params=None):
@@ -20,7 +22,7 @@ def api_get_url_json(url, access_token, params=None):
                    'Authorization': f'Token {access_token}'}
     if params is None:
         params = {}
-    response = requests.get(url, headers=api_headers, params={**params, 'format': 'json'})
+    response = requests.get(url, headers=api_headers, **params)
     if response.status_code != 200:
         print(f'Requested: {response.url}')
         print(f'Response: {response}')
@@ -35,7 +37,19 @@ def api_get_url_json(url, access_token, params=None):
     return response.json()
 
 
-def find_stuck_submissions(access_token, api_url, course_id):
+def api_get_paginated_json(url, access_token, params=None):
+    """
+    Fetch a paginated JSON file from the A+ API.
+    """
+    results = []
+    while url is not None:
+        data = api_get_url_json(url, access_token, params)
+        results.extend(data['results'])
+        url = data['next']
+    return results
+
+
+def find_stuck_submissions(access_token, api_url, course_id, since: int):
     """
     Find all stuck (not "ready" or "rejected) submissions in an A+ course.
     Return the list of their "inspect" URLs.
@@ -45,29 +59,38 @@ def find_stuck_submissions(access_token, api_url, course_id):
         """Get a JSON file from the A+ API."""
         return api_get_url_json(url, access_token, params)
 
+    def api_get_paginated(url, params=None):
+        """Get a paginated JSON file from the A+ API."""
+        return api_get_paginated_json(url, access_token, params)
+
+    now_time = datetime.datetime.astimezone(datetime.datetime.now())
+
     print("Fetching the exercises index")
     api_exercises_url = f'{api_url}/courses/{course_id}/exercises'
-    exercises_json = api_get_json(api_exercises_url)
+    exercises_json = api_get_paginated(api_exercises_url)
     time.sleep(1)
-
     stuck_urls = []
-    for round_json in exercises_json['results']:
+    for round_json in exercises_json:
         print(f'Scanning round: {round_json["display_name"]}')
         for exercise_json in round_json['exercises']:
-            exercise_id = exercise_json['id']
+            exercise_json = api_get_json(exercise_json['url'])
             exercise_name = exercise_json['display_name']
-            exercise_html = exercise_json['html_url']
             print(f'  Scanning exercise: {exercise_name}')
-            submissions_json = api_get_json(
-                f'{api_url}/courses/{course_id}/submissiondata/?exercise_id={exercise_id}',
-                {'best': 'no'})
+            submissions_json = api_get_paginated(exercise_json['submissions'])
             for submission_json in submissions_json:
-                submission_id = submission_json['SubmissionID']
-                submission_status = submission_json['Status']
-                inspect_url = f'{exercise_html}/submissions/{submission_id}/inspect/'
-                if submission_status not in ['ready', 'rejected']:
-                    stuck_urls.append(inspect_url)
-            time.sleep(1)
+                if submission_json['grade'] != 0:
+                    continue
+                submission_time = timeparser(submission_json['submission_time'])
+                if (now_time - submission_time).days >= since:
+                    continue
+                submission_json = api_get_json(submission_json['url'])
+                time.sleep(0.5)
+                if submission_json['status'] in ['ready', 'rejected']:
+                    continue
+                inspect_url = submission_json['html_url']+'inspect/'
+                stuck_urls.append(inspect_url)
+                # print(inspect_url)
+                # print(submission_json)
     return stuck_urls
 
 
@@ -75,13 +98,18 @@ def main():
     """
     The main function for command line use.
     """
-    description = 'Find all submissions in an A+ course that are not "ready" or "rejected".'
+    description = ('Find all recent submissions in an A+ course that are '
+                   'not "ready" or "rejected".')
     argp = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         description=description)
+    argp.add_argument('--since', type=int, default=1, metavar='S',
+                      help=('only consider submissions that are less than '
+                            'S days old'))
     argp.add_argument('--config', default='find_stuck_submissions.yml',
                       metavar='C',
-                      help='The config YAML file with access token and course id')
+                      help=('The config YAML file with the access token and '
+                            'the course id'))
     argp.add_argument('--urls-file', default='stuck_submissions.txt',
                       metavar='F',
                       help=('The "inspect" URLs of the stuck submissions '
@@ -100,7 +128,8 @@ def main():
         argp.error('The course-id in the config YAMl file should be an integer')
 
     # Find the stuck submissions and get their "inspect" URLS
-    stuck_urls = find_stuck_submissions(access_token, api_url, course_id)
+    stuck_urls = find_stuck_submissions(access_token, api_url, course_id,
+                                        args.since)
 
     # Write the "inspect" URLS in a file
     with open(args.urls_file, 'w', encoding='utf-8') as urls_fh:
